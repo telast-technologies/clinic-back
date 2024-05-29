70bb092679ede80daaaea01a6a99cf653e73f2fd
hateemsoftylus <ahmed.hatem@softylus.com>
2024-05-29 09:02:51 +0300

    feat: update package fsm to viewflow

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,8 @@
 from config.settings.base import *  # noqa
-from config.settings.base import env
+from config.settings.base import MIDDLEWARE, env
+
+# SECURITY WARNING: don't run with debug turned on in production!
+MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -7,8 +10,13 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 
 # STATIC
 # ------------------------
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
-    }
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
