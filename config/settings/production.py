f9c1d2914792594caee1749484220181dc9dfced
hateemsoftylus <ahmed.hatem@softylus.com>
2024-05-30 00:55:16 +0300

    feat: update package fsm to viewflow

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,6 +7,21 @@ INSTALLED_APPS += ["whitenoise.runserver_nostatic"]
 MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"]
 # SECURITY
 # ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 CSRF_TRUSTED_ORIGINS = ["https://*.clinic.telast.tech"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
 CORS_ALLOWED_ORIGINS = ["https://*.clinic.telast.tech"]
