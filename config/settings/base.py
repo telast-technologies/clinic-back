e63549476b4c154fbd47f01b5bfdfefb0e651ee0
hateemsoftylus <ahmed.hatem@softylus.com>
2024-06-17 20:34:39 +0300

    feat: upgrade clinic

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,7 +94,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "config.middleware.ClinicWhiteListMiddleware",
+    # "config.middleware.ClinicWhiteListMiddleware",
 ]
 
 ROOT_URLCONF = "clinic.urls"
