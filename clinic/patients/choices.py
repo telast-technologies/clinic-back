aba7ac99558918617edd0dc7db12d99f545e693b
el-hate <elhaatem@gmail.com>
2024-07-06 15:12:40 +0300

    feat: add nid & channel to patient admission

--- clinic/patients/choices.py
+++ clinic/patients/choices.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Channels(models.TextChoices):
     FACEBOOK = "facebook", _("Facebook")
     TELEGRAM = "telegram", _("Telegram")
