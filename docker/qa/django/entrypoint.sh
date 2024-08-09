00462c090e504cd5ba7849fffe42aa534cb85b87
el-hate <elhaatem@gmail.com>
2024-08-09 22:01:27 +0300

    feat: push v1

--- docker/qa/django/entrypoint.sh
+++ docker/qa/django/entrypoint.sh
@@ -6,11 +6,11 @@ set -o nounset
 
 
 
-if [ -z "${POSTGRES_USER}" ]; then
+if [ -z "${CLINIC_POSTGRES_USER}" ]; then
     base_postgres_image_default_user='postgres'
-    export POSTGRES_USER="${base_postgres_image_default_user}"
+    export CLINIC_POSTGRES_USER="${base_postgres_image_default_user}"
 fi
-export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+export DATABASE_URL="postgres://${CLINIC_POSTGRES_USER}:${CLINIC_POSTGRES_PASSWORD}@${CLINIC_POSTGRES_HOST}:${CLINIC_POSTGRES_PORT}/${CLINIC_POSTGRES_DB}"
 
 python << END
 
@@ -25,11 +25,11 @@ start = time.time()
 while True:
     try:
         psycopg2.connect(
-            dbname="${POSTGRES_DB}",
-            user="${POSTGRES_USER}",
-            password="${POSTGRES_PASSWORD}",
-            host="${POSTGRES_HOST}",
-            port="${POSTGRES_PORT}",
+            dbname="${CLINIC_POSTGRES_DB}",
+            user="${CLINIC_POSTGRES_USER}",
+            password="${CLINIC_POSTGRES_PASSWORD}",
+            host="${CLINIC_POSTGRES_HOST}",
+            port="${CLINIC_POSTGRES_PORT}",
         )
         break
     except psycopg2.OperationalError as error:
