5dcd968b82748b28ffd727253d4bad92ad002b61
el-hate <elhaatem@gmail.com>
2024-10-18 03:00:04 +0300

    feat: imporve filter patient

--- clinic/patients/api/v1/views.py
+++ clinic/patients/api/v1/views.py
@@ -1,8 +1,9 @@
-from drf_spectacular.utils import OpenApiParameter, OpenApiTypes, extend_schema, extend_schema_view
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, mixins, viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 from clinic.patients.api.v1.serializers import PatientReportSerializer, PatientSerializer, SelectPatientSerializer
-from clinic.patients.filters import PatientFilter, PatientReportFilter, SelectPatientFilter
+from clinic.patients.filters import PatientFilter, PatientReportFilter
 from clinic.patients.models import Patient, PatientReport
 from clinic.users.abstracts.mixins import QuerysetFilteredMixin
 from clinic.users.api.permissions import IsStaff
@@ -11,24 +12,21 @@ from clinic.users.api.permissions import IsStaff
 class PatientViewSet(QuerysetFilteredMixin, viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     queryset = Patient.objects.all()
-    filterset_class = PatientFilter
     permission_classes = [IsStaff]
+    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+    filterset_class = PatientFilter
+    search_fields = ["first_name", "last_name", "email", "phone", "nid", "uid"]
+    ordering_fields = ["created_at"]
 
 
 class SelectPatientView(QuerysetFilteredMixin, generics.ListAPIView):
     serializer_class = SelectPatientSerializer
     queryset = Patient.objects.all()
-    filterset_class = SelectPatientFilter
     permission_classes = [IsStaff]
+    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+    search_fields = ["first_name", "last_name", "email", "phone", "nid", "uid"]
 
 
-@extend_schema_view(
-    destroy=extend_schema(
-        parameters=[
-            OpenApiParameter(name="patient", description="Patient ID", required=True, type=OpenApiTypes.UUID),
-        ]
-    ),
-)
 class PatientReportViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
@@ -37,8 +35,9 @@ class PatientReportViewSet(
 ):
     serializer_class = PatientReportSerializer
     queryset = PatientReport.objects.all()
-    filterset_class = PatientReportFilter
     permission_classes = [IsStaff]
+    filter_backends = [SearchFilter, OrderingFilter, DjangoFilterBackend]
+    filterset_class = PatientReportFilter
 
     def get_queryset(self):
         return super().get_queryset().filter(patient__clinic=self.request.user.staff.clinic)
--- clinic/patients/filters.py
+++ clinic/patients/filters.py
@@ -4,39 +4,14 @@ from clinic.patients.models import Patient, PatientReport
 
 
 class PatientFilter(django_filters.FilterSet):
-    fullname = django_filters.CharFilter(
-        method="filter_fullname",
-    )
     created_at = django_filters.DateTimeFromToRangeFilter(field_name="created_at")
 
     class Meta:
         model = Patient
-        fields = ("uid", "fullname", "phone", "email", "created_at", "channel", "nid")
-
-    def filter_fullname(self, queryset, name, value):
-        return queryset.filter(first_name__icontains=value) | queryset.filter(last_name__icontains=value)
-
-
-class SelectPatientFilter(django_filters.FilterSet):
-    fullname = django_filters.CharFilter(
-        method="filter_fullname",
-    )
-
-    class Meta:
-        model = Patient
-        fields = ("fullname", "uid")
-
-    def filter_fullname(self, queryset, name, value):
-        return queryset.filter(first_name__icontains=value) | queryset.filter(last_name__icontains=value)
+        fields = ("created_at", "channel")
 
 
 class PatientReportFilter(django_filters.FilterSet):
-    patient = django_filters.ModelChoiceFilter(
-        queryset=Patient.objects.all(),
-        field_name="patient",
-        label="Staff can see only reports of their Patient",
-        required=True,
-    )
     created_at = django_filters.DateTimeFromToRangeFilter(field_name="created_at")
 
     class Meta:
