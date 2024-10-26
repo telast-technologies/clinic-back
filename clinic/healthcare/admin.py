e276eb4cd22ee0fec8397d19165df1e6110c875d
el-hate <elhaatem@gmail.com>
2024-10-26 11:28:30 +0300

    fix: refactor service app

--- clinic/healthcare/admin.py
+++ clinic/healthcare/admin.py
@@ -1,7 +1,4 @@
-from typing import Any
-
 from django.contrib import admin
-from django.http.request import HttpRequest
 
 from clinic.healthcare.models import Service
 
@@ -19,12 +16,3 @@ class ServiceAdmin(admin.ModelAdmin):
         "clinic",
     )
     search_fields = ("name", "uid")
-
-    def has_add_permission(self, request: HttpRequest, obj: Any = None) -> bool:
-        return False
-
-    def has_change_permission(self, request: HttpRequest, obj: Any = None) -> bool:
-        return False
-
-    def has_delete_permission(self, request: HttpRequest, obj: Any = None) -> bool:
-        return False
