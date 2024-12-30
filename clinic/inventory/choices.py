574b439e1a9b22896b5670738a449bec507023a1
el-hatem <analyst.ahmedhatem@hotmail.com>
2024-12-30 06:54:51 +0200

    fix: refactor inventory, service modules

--- clinic/inventory/choices.py
+++ clinic/inventory/choices.py
@@ -1,8 +1,7 @@
 from django.db import models
-
 from django.utils.translation import gettext_lazy as _
 
 
 class SupplyType(models.TextChoices):
-    DRUG = "drug", _('Drug')
-    SUPPLEMENT = "supplement", _('Supplement')
+    DRUG = "drug", _("Drug")
+    SUPPLEMENT = "supplement", _("Supplement")
