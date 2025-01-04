65ef1b38693fecd5ed1ae50d6aac01faeaf2b4ef
el-hatem <analyst.ahmedhatem@hotmail.com>
2025-01-04 13:40:00 +0200

    fix(invoice filter): adding due date

--- clinic/invoices/filters.py
+++ clinic/invoices/filters.py
@@ -5,10 +5,11 @@ from clinic.invoices.models import ChargeItem, ChargeService, Invoice
 
 class InvoiceFilter(django_filters.FilterSet):
     created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
+    due_date = django_filters.DateFromToRangeFilter(field_name="due_date")
 
     class Meta:
         model = Invoice
-        fields = ("visit", "created_at")
+        fields = ("visit", "created_at", "due_date")
 
 
 class ChargeItemFilter(django_filters.FilterSet):
