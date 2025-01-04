721690f88baa884ad77ae4f5a6527e45b26cbfdf
el-hatem <analyst.ahmedhatem@hotmail.com>
2025-01-04 12:24:37 +0200

    fix(invoice): adding status, comment

--- clinic/invoices/choices.py
+++ clinic/invoices/choices.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class InvoiceStatus(models.TextChoices):
-    PAID = "PAID", _("Paid")
-    PARTIAL_PAID = "PARTIAL_PAID", _("Partial Paid")
-    UNPAID = "UNPAID", _("Unpaid")
-    CANCELLED = "CANCELLED", _("Cancelled")
+    PAID = "paid", _("Paid")
+    PARTIAL_PAID = "partial_paid", _("Partial Paid")
+    UNPAID = "unpaid", _("Unpaid")
+    CANCELLED = "cancelled", _("Cancelled")
