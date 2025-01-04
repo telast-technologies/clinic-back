98e6deffe2fce5ceaac496fe397a8da89021eb7c
el-hatem <analyst.ahmedhatem@hotmail.com>
2025-01-04 13:41:02 +0200

    fix(invoice): allow search by visit no

--- clinic/invoices/api/v1/views.py
+++ clinic/invoices/api/v1/views.py
@@ -54,6 +54,7 @@ class SelectInvoiceView(QuerysetFilteredMixin, generics.ListAPIView):
     search_fields = [
         "uid",
         "no",
+        "visit__no"
         "visit__patient__medical_number",
         "visit__patient__first_name",
         "visit__patient__last_name",
