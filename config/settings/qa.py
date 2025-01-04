bf24caeecc2345901120e1a2b173d9c7ca823fb9
el-hatem <analyst.ahmedhatem@hotmail.com>
2025-01-04 04:01:32 +0200

    fix: change domain

--- config/settings/qa.py
+++ config/settings/qa.py
@@ -12,7 +12,7 @@ CACHES = {
 # SECURITY
 # ------------------------------------------------------------------------------
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = ["https://*.clinic.telast.tech"]
+CSRF_TRUSTED_ORIGINS = ["https://*.clinic.telasttechnologies.com"]
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
