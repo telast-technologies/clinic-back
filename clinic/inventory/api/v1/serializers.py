1cc8cb6fa5d419167d8da71f9d511ae83e861b5d
el-hatem <analyst.ahmedhatem@hotmail.com>
2025-01-21 17:17:53 +0200

    fix: select supply

--- clinic/inventory/api/v1/serializers.py
+++ clinic/inventory/api/v1/serializers.py
@@ -24,7 +24,7 @@ class SelectSupplySerializer(serializers.ModelSerializer):
     label = serializers.CharField(source="item", read_only=True)
     value = serializers.CharField(source="uid", read_only=True)
     unit_sales_price = serializers.FloatField(read_only=True)
-
+    remains = serializers.FloatField(read_only=True)
     class Meta:
         model = Supply
-        fields = ("label", "value", "description", "unit_sales_price", "quantity")
+        fields = ("label", "value", "description", "unit_sales_price", "remains")
